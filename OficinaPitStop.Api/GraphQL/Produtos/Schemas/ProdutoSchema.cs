b531c04105d59a46211d12fc81c8e78009bf3cd5
LuizGPG <luizperesg@gmail.com>
2020-04-19 17:30:37 -0300

    Criando Schema

---
+++ OficinaPitStop.Api/GraphQL/Produtos/Schemas/ProdutoSchema.cs
@@ -0,0 +1,13 @@
+using GraphQL;
+using GraphQL.Types;
+
+namespace OficinaPitStop.Api.GraphQL.Produtos.Schemas
+{
+    public class ProdutoSchema : Schema
+    {
+        public ProdutoSchema()
+        {
+            
+        }
+    }
+}
